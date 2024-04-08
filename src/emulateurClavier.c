37e7cfcf69dbb50631bdc7c6cef17067c241555f
Xavier Hamel <hamelxavier.2@gmail.com>
2024-04-08 11:53:30 -0400

    extended ascii to usb-hid conversion table

--- src/emulateurClavier.c
+++ src/emulateurClavier.c
@@ -95,25 +95,39 @@ unsigned int ascii2UsbHid(char ascii) {
 
     switch (ascii)
     {
-    case '0':
-        return 39;
-        break;
-
-    case '.':
-        return 55;
-        break;
-    
-    case ',':
-        return 54;
-        break;
-
-    case ' ':
-        return 44;
-        break;
-
-    case '\n':
-        return 40;
-        break;
+    case '0':  return 0x27;
+    case '.':  return 0x37;
+    case ',':  return 0x36;
+    case ' ':  return 0x2C;
+    case '\n': return 0x28;//newline
+    case '\r': return 0x28;//carriage return
+    case '!' : return 0x11E;
+    case '@' : return 0x11F;
+    case '#' : return 0x120;
+    case '$' : return 0x121;
+    case '%' : return 0x122;
+    case '&' : return 0x124;
+    case '*' : return 0x125;
+    case '(' : return 0x126;
+    case ')' : return 0x127;
+    case '_' : return 0x12D;
+    case '-' : return 0x2D;
+    case '+' : return 0x12E;
+    case '=' : return 0x2E;
+    case '{' : return 0x12F;
+    case '}' : return 0x130;
+    case '[' : return 0x2F;
+    case ']' : return 0x30;
+    case '\\': return 0x31;
+    case ':' : return 0x133;
+    case ';' : return 0x33;
+    case '"' : return 0x134;
+    case '\'': return 0x34;
+    case '~' : return 0x135;
+    case '<' : return 0x136;
+    case '>' : return 0x137;
+    case '?' : return 0x138;
+    case '/' : return 0x38;
 
     default:
         if (ascii >= '1' && ascii <= '9') {
@@ -124,8 +138,7 @@ unsigned int ascii2UsbHid(char ascii) {
         }
         else if (ascii >= 'A' && ascii <= 'Z') {
             return (num_value - 61 + 225);
-        } 
-        break;
+        }
     }
     
     return 0;
